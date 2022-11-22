f6bd4754b73bdfc059eb4c84bfe1f7a60b173332
Xavier Hillman <xhillman13@gmail.com>
2022-11-22 11:47:29 -0500

    integrate landing page into router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-=======
 // import App from './App';
 import Home from './Components/Home';
->>>>>>> 379b063 (integrate landing page into router)
 import Companies from './Components/Companies/table';
 import Roles from './Components/Roles/form';
 import People from './Components/People/form';
@@ -44,21 +40,13 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <RouterProvider router = {router} />
-    
-=======
->>>>>>> 379b063 (integrate landing page into router)
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
     >
-<<<<<<< HEAD
-=======
       <RouterProvider router={router} />
       {/* <App /> */}
->>>>>>> 379b063 (integrate landing page into router)
     </Auth0Provider>
   </React.StrictMode>
 );
