19fd3c567c8c810d878322bb8614aab0e98fc5f9
nurselaine <Elaine.t.huynh@gmail.com>
2022-11-22 01:55:48 -0800

    commented in basic RTK boilerplate code and added comments to get data for companies table component

---
+++ src/Store/index.js
@@ -0,0 +1,13 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import jobReducer from './reducers/jobsReducer';
+
+// const store = configureStore({
+//   reducer: {
+//     jobReducer,
+//   },
+// });
+
+// export default store;
+
+
+
