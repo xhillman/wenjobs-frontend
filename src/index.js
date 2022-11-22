a6838c0db4334227164c7d8d72a373fc718a1304
Xavier Hillman <xhillman13@gmail.com>
2022-11-22 12:14:20 -0500

    resolve git conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import App from './App';
+=======
+// import App from './App';
+import Home from './Components/Home';
+>>>>>>> 379b063 (integrate landing page into router)
 import Companies from './Components/Companies/table';
 import Roles from './Components/Roles/form';
 import People from './Components/People/form';
@@ -10,12 +15,13 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Home />,
     errorElement: <ErrorPage />
   },
   {
@@ -38,13 +44,21 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <RouterProvider router = {router} />
     
+=======
+>>>>>>> 379b063 (integrate landing page into router)
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
     >
+<<<<<<< HEAD
+=======
+      <RouterProvider router={router} />
+      {/* <App /> */}
+>>>>>>> 379b063 (integrate landing page into router)
     </Auth0Provider>
   </React.StrictMode>
 );
