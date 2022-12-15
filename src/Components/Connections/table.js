ba2991054e200d4a18cc333067a4d359e4bc10ab
Jacks-01 <Jack011601@gmail.com>
2022-12-14 20:58:28 -0600

    made connections data pull on page load if user is authenticated

--- src/Components/Connections/table.js
+++ src/Components/Connections/table.js
@@ -1,40 +1,22 @@
-import { Table, Tag } from 'antd';
+import { Table } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, message, Upload, List, UploadProps, Form, Input } from 'antd';
-import { initializeApp } from 'firebase/app';
-import { getDocFromCache, getFirestore } from 'firebase/firestore';
-import { collection, addDoc, getDoc, writeBatch, doc, updateDoc } from 'firebase/firestore';
-import React, { useState, useEffect } from 'react';
+import { Button, Upload, Form, Input } from 'antd';
+import { getDoc, doc, updateDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import Column from 'antd/es/table/Column';
 import { useAuth0 } from "@auth0/auth0-react";
 import './style.css'
+import db from '../Firebase/FirebaseConfig';
 
 
 const PeopleTable = () => {
 
   const { user, isAuthenticated } = useAuth0();
 
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  };
-
-
 
   const [connectionsData, setConnectionsData] = useState(null);
   const [searchText, setSearchText] = useState(null);
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
 
   const handleFile = (info) => {
 
@@ -70,7 +52,7 @@ const PeopleTable = () => {
 
 
     const docRef = doc(db, "users", user.email);
-    let docSnap = await getDocFromCache(docRef);
+    let docSnap = await getDoc(docRef);
     let data;
 
     if (docSnap.exists()) {
@@ -87,48 +69,53 @@ const PeopleTable = () => {
     setConnectionsData(data);
   }
 
-  return (
-    <>
-      <div className='peopleFormWrapper'>
-        <Form
-          labelCol={{ span: 15 }}
-          wrapperCol={{ span: 20 }}
-          layout="vertical"
-        >
-          <Form.Item label="Search by First Name">
-            <Input.Search onSearch={(value) => {
-              setSearchText(value);
-            }} />
-          </Form.Item>
-          <Form.Item label="Search by Last Name">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Search by Company">
-            <Input />
-          </Form.Item>
-        </Form>
-      </div >
-      <div className='connectionsTableWrapper'>
-        <Table pagination={{ pageSize: '5' }}
-          defaultPageSize={5}
-          dataSource={connectionsData}>
-          <Column title='First Name' dataIndex='First Name' key={Math.random()} />
-          <Column title='Last Name' dataIndex='Last Name' key={Math.random()} />
-          <Column title='Email Address' dataIndex='Email Address' key={Math.random()} />
-          <Column title='Company' dataIndex='Company' key={Math.random()} />
-          <Column title='Position' dataIndex='Position' key={Math.random()} />
-          <Column title='Connected On' dataIndex='Connected On' key={Math.random()} />
-        </Table>
-        <div className='uploadSectionWrapper'>
-          <Upload customRequest={handleFile}>
-            <Button className='uploadCSVButton' icon={<UploadOutlined />}>Upload CSV</Button>
-            <Button className='contentModifyButton' onClick={addConnectionsData}>Add Connections to Database</Button>
-            <Button className='contentModifyButton' onClick={readConnectionsData}>Get Connections from Database</Button>
-          </Upload>
+  useEffect(() => {
+    if (isAuthenticated) {
+      readConnectionsData();
+    }
+  }, [isAuthenticated])
+    return (
+      <>
+        <div className='peopleFormWrapper'>
+          <Form
+            labelCol={{ span: 15 }}
+            wrapperCol={{ span: 20 }}
+            layout="vertical"
+          >
+            <Form.Item label="Search by First Name">
+              <Input.Search onSearch={(value) => {
+                setSearchText(value);
+              }} />
+            </Form.Item>
+            <Form.Item label="Search by Last Name">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Search by Company">
+              <Input />
+            </Form.Item>
+          </Form>
+        </div >
+        <div className='connectionsTableWrapper'>
+          <Table pagination={{ pageSize: '5' }}
+            defaultPageSize={5}
+            dataSource={connectionsData}>
+            <Column title='First Name' dataIndex='First Name' key={Math.random()} />
+            <Column title='Last Name' dataIndex='Last Name' key={Math.random()} />
+            <Column title='Email Address' dataIndex='Email Address' key={Math.random()} />
+            <Column title='Company' dataIndex='Company' key={Math.random()} />
+            <Column title='Position' dataIndex='Position' key={Math.random()} />
+            <Column title='Connected On' dataIndex='Connected On' key={Math.random()} />
+          </Table>
+          <div className='uploadSectionWrapper'>
+            <Upload customRequest={handleFile}>
+              <Button className='uploadCSVButton' icon={<UploadOutlined />}>Upload CSV</Button>
+              <Button className='contentModifyButton' onClick={addConnectionsData}>Add Connections to Database</Button>
+              <Button className='contentModifyButton' onClick={readConnectionsData}>Get Connections from Database</Button>
+            </Upload>
+          </div>
         </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
 
 export default PeopleTable;
