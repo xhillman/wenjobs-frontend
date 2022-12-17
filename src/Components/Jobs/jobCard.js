ad3e4693712f7b6e236544a42f41576b1ff3a8ba
Jacks-01 <Jack011601@gmail.com>
2022-12-17 16:08:47 -0600

    styled the card more

--- src/Components/Jobs/jobCard.js
+++ src/Components/Jobs/jobCard.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Card } from "antd";
+import { Button, Card, Empty, Space } from "antd";
 import { useSelector } from "react-redux";
+import { Typography } from "antd";
+const { Title, Text, Paragraph } = Typography;
 
 
 
 const JobCard = () => {
     let selectedJob = useSelector((state) => state.jobs.selectedJob);
     return (
-        <Card  bodyStyle={{ overflowY: 'auto', height: '67.5vh' }}>
-            <h2>{selectedJob.title}</h2>
-            <p>{selectedJob.details}</p>
+        <Card
+            bodyStyle={{ overflowY: 'auto', height: '60vh' }}
+            actions={[
+                <Button type="primary">Apply Here</Button>
+            ]}
+        >
+            <Title level={2} >{selectedJob.title}</Title>
+            <Text strong style={{fontSize: '1.2rem'}}> Company: {selectedJob.company}</Text>
+            <Text strong style={{fontSize: '1.2rem', display: 'block', paddingBottom: '1rem'}}> Location: {selectedJob.location}</Text>
+
+            <Paragraph style={{fontSize: '1rem'}}>{selectedJob.details}</Paragraph>
 
         </Card>
 
