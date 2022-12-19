d6b908ad67519f778cd154562a0379deceff1f5f
Jack Stubblefield <Jack011601@gmail.com>
2022-12-19 11:40:57 -0600

    removed previous layout from connections

--- src/Components/Connections/layout.js
+++ src/Components/Connections/layout.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import { Layout } from 'antd';
 import PeopleTable from './table';
-const { Content } = Layout;
+import PeopleForm from './form';
+const { Content, Sider } = Layout;
 
 function ConnectionsLayout() {
 
   return (
     <Layout>
-      <Layout>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <div className='peopleContentWrapper'>
-              {/* <PeopleForm /> */}
-              <PeopleTable />
-            </div>
-          </Content>
-        </Layout>
-      </Layout>
+
+      <PeopleForm />
+      <PeopleTable />
     </Layout>
+
+
   );
 }
 
