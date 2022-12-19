27e347b4e5ca7cbc854ec35785087cbbcf1a3cef
Jack Stubblefield <Jack011601@gmail.com>
2022-12-19 14:28:30 -0600

    connected companies table to browserRouter

--- src/Components/Companies/layout.js
+++ src/Components/Companies/layout.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import { Layout } from 'antd';
-import PeopleTable from './table';
+import CompaniesTable from './table';
 const { Content } = Layout;
 
-function ConnectionsLayout() {
+function CompaniesLayout() {
 
   return (
-    <Layout>
-      <Layout>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <div className='peopleContentWrapper'>
-              {/* <PeopleForm /> */}
-              <PeopleTable />
-            </div>
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
+
+    <Layout className='layout'>
+
+      <Content className='content'>
+        <CompaniesTable />
+      </Content>
+
+    </Layout >
+
   );
 }
 
-export default ConnectionsLayout;
+export default CompaniesLayout;
