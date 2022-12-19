f724ff529861335b3766142daa19f812f0db4132
Jack Stubblefield <Jack011601@gmail.com>
2022-12-19 11:49:08 -0600

    added classNames and css file to connections layout

--- src/Components/Connections/layout.js
+++ src/Components/Connections/layout.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { Layout } from 'antd';
 import PeopleTable from './table';
 import PeopleForm from './form';
+import './style.css';
 const { Content, Sider } = Layout;
-
 function ConnectionsLayout() {
 
   return (
-    <Layout>
+    <Layout className='layout'>
 
-      <PeopleForm />
+      <Sider className='sider' width='20rem' theme='light'>
+        <PeopleForm />
+      </Sider>
+
+      <Content className='content'>
       <PeopleTable />
-    </Layout>
+    </Content>
+
+    </Layout >
 
 
   );
