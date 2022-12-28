a66f3006e5279030c26437aadaf89fd04f0bf477
Xavier Hillman <xhillman13@gmail.com>
2022-12-28 13:13:03 -0500

    add search by company to jobs page

--- src/Components/Jobs/table.js
+++ src/Components/Jobs/table.js
@@ -1,109 +1,218 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Space, Tag } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Table, Space, Tag, Input, Button } from 'antd';
 import { limit, orderBy, query } from 'firebase/firestore';
 import { collection, getDocs, startAfter } from 'firebase/firestore';
 import db from '../Firebase/FirebaseConfig';
 import { useSelector, useDispatch } from 'react-redux';
 import { setJobs, setSelectedJob } from '../../Store/slices/jobs';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 
 import './style.css'
 
-const columns = [
-  {
-    title: 'Job Title',
-    dataIndex: 'title',
-    key: 'title',
-  },
-  {
-    title: 'Posted',
-    dataIndex: 'posted',
-    key: 'posted',
-  },
-  {
-    title: 'Company',
-    dataIndex: 'company',
-    key: 'company',
-  },
-  {
-    title: 'Location',
-    dataIndex: 'location',
-    key: 'location',
-  },
-  {
-    title: 'Salary',
-    dataIndex: 'salary',
-    key: 'salary',
-    width: '10% '
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {
-
-          tags.map((tag) => {
-            let color;
-            switch (tag.length) {
-              case 2:
-                color = 'geekblue';
-                break;
-              case 3:
-                color = 'green';
-                break;
-              case 4:
-                color = 'blue';
-                break;
-              case 5:
-                color = 'purple';
-                break;
-              case 6:
-                color = 'orange';
-                break;
-              case 7:
-                color = 'magenta';
-                break;
-              case 8:
-                color = 'cyan';
-                break;
-              case 9:
-                color = 'volcano';
-                break;
-              case 10:
-                color = 'gold';
-                break;
-              default:
-                color = '';
-                break;
-            }
-
-            return (
-              <Space wrap>
-                <Tag color={color} key={tag}>
-                  {tag.toUpperCase()}
-                </Tag>
-              </Space>
-            );
-          })}
-      </>
-    ),
-  },
-  // {
-  //   title: 'Apply Now!',
-  //   dataIndex: 'URL',
-  //   key: 'URL',
-  //   render: (link) => <Button type="primary" href={link} target='_blank'>Apply!</Button>
-  // },
-];
-
 function RoleTable() {
+
 
   const jobs = useSelector(state => state.jobs);
   const dispatch = useDispatch();
 
   const [lastVisible, setLastVisible] = useState(null);
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
 
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8, }} onKeyDown={(e) => e.stopPropagation()} >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              handleReset(clearFilters);
+              handleSearch(selectedKeys, confirm, dataIndex);
+            }
+            }
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{ color: filtered ? '#1890ff' : undefined, }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#fff',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: 'Job Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Posted',
+      dataIndex: 'posted',
+      key: 'posted',
+    },
+    {
+      title: 'Company',
+      dataIndex: 'company',
+      key: 'company',
+      ...getColumnSearchProps('company'),
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Salary',
+      dataIndex: 'salary',
+      key: 'salary',
+      width: '10% '
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {
+
+            tags.map((tag) => {
+              let color;
+              switch (tag.length) {
+                case 2:
+                  color = 'geekblue';
+                  break;
+                case 3:
+                  color = 'green';
+                  break;
+                case 4:
+                  color = 'blue';
+                  break;
+                case 5:
+                  color = 'purple';
+                  break;
+                case 6:
+                  color = 'orange';
+                  break;
+                case 7:
+                  color = 'magenta';
+                  break;
+                case 8:
+                  color = 'cyan';
+                  break;
+                case 9:
+                  color = 'volcano';
+                  break;
+                case 10:
+                  color = 'gold';
+                  break;
+                default:
+                  color = '';
+                  break;
+              }
+
+              return (
+                <Space wrap>
+                  <Tag color={color} key={tag}>
+                    {tag.toUpperCase()}
+                  </Tag>
+                </Space>
+              );
+            })}
+        </>
+      ),
+    },
+    // {
+    //   title: 'Apply Now!',
+    //   dataIndex: 'URL',
+    //   key: 'URL',
+    //   render: (link) => <Button type="primary" href={link} target='_blank'>Apply!</Button>
+    // },
+  ];
   // declaring variables from redux state
   let jobsData = [...jobs.filteredJobs];
 
@@ -138,7 +247,7 @@ function RoleTable() {
   // fetch the first 30 jobs on page load
   useEffect(() => {
     fetchJobs();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleRowSelection = {
