a88f204d8dbe97d5688e91990d28dd3c8e38b81d
Xavier Hillman <xhillman13@gmail.com>
2022-12-28 16:13:31 -0500

    add upload instructions and add search functionality

--- src/Components/Connections/table.js
+++ src/Components/Connections/table.js
@@ -1,16 +1,21 @@
-import { Table } from 'antd';
+import { Table, Input, Space, Button } from 'antd';
 import { getDoc, doc } from 'firebase/firestore';
-import React, { useEffect } from 'react';
-import Column from 'antd/es/table/Column';
+import React, { useEffect, useRef, useState } from 'react';
+// import Column from 'antd/es/table/Column';
 import { useAuth0 } from "@auth0/auth0-react";
 import './style.css'
 import db from '../Firebase/FirebaseConfig';
 import { useSelector, useDispatch } from 'react-redux';
 import { setConnectionsData } from '../../Store/slices/connections';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 
 const PeopleTable = () => {
 
   const { user, isAuthenticated } = useAuth0();
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
 
   let connectionsData = useSelector(state => state.connections.connections);
   const dispatch = useDispatch();
@@ -36,6 +41,143 @@ const PeopleTable = () => {
     dispatch(setConnectionsData(data));
   }
 
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8, }} onKeyDown={(e) => e.stopPropagation()} >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              handleReset(clearFilters);
+              handleSearch(selectedKeys, confirm, dataIndex);
+            }
+            }
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{ color: filtered ? '#1890ff' : undefined, }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#fff',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: 'First Name',
+      dataIndex: 'First Name',
+      key: 'firstName',
+      width: '15%',
+      ...getColumnSearchProps('First Name'),
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'Last Name',
+      key: 'lastName',
+      width: '15%',
+      ...getColumnSearchProps('Last Name'),
+    },
+    {
+      title: 'Company',
+      dataIndex: 'Company',
+      key: 'company',
+      ...getColumnSearchProps('Company'),
+    },
+    {
+      title: 'Position',
+      dataIndex: 'Position',
+      key: 'position',
+      ...getColumnSearchProps('Position'),
+    },
+    {
+      title: 'Connected On',
+      dataIndex: 'Connected On',
+      key: 'connected',
+    }
+  ]
+
   useEffect(() => {
     if (isAuthenticated) {
       readConnectionsData();
@@ -49,13 +191,8 @@ const PeopleTable = () => {
       <Table pagination={{ pageSize: '10' }}
         defaultPageSize={10}
         style={{ height: '100%' }}
-        dataSource={connectionsData}>
-        <Column title='First Name' dataIndex='First Name' key={Math.random()} />
-        <Column title='Last Name' dataIndex='Last Name' key={Math.random()} />
-        <Column title='Email Address' dataIndex='Email Address' key={Math.random()} />
-        <Column title='Company' dataIndex='Company' key={Math.random()} />
-        <Column title='Position' dataIndex='Position' key={Math.random()} />
-        <Column title='Connected On' dataIndex='Connected On' key={Math.random()} />
+        dataSource={connectionsData}
+        columns={columns}>
       </Table>
 
 
